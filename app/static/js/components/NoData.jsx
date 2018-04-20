bd98821d8068e6b7965db1ca2f9e61df614fabf9
Bony Roopchandani <broopchandani@datalicious.com>
2018-04-21 04:33:29 +0530

    no data height  On branch master 	modified:   app/static/js/components/NoData.jsx

--- app/static/js/components/NoData.jsx
+++ app/static/js/components/NoData.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default class NoData extends React.Component {
     render () {
+        let height = this.props.style ? this.props.style.height || "200px" : "200px"
         return (
-            <div style={{ height: "200px", lineHeight: "200px", textAlign: "center" }}>
+            <div style={{ height: height, lineHeight: "200px", textAlign: "center" }}>
                 {`No ${this.props.name || "data"}  found!`}
             </div>
         )
