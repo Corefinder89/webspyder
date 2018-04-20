b8f9ed202d09f995077dbac472f71f73836134c6
corefinder <soumyajit.basu62@gmail.com>
2018-04-20 19:54:59 +0530

    Add codebase for desktop, mobile and base

--- app/core/spider/mobile.py
+++ app/core/spider/mobile.py
@@ -4,14 +4,15 @@ from selenium.common.exceptions import WebDriverException
 from app.core.spider.base import Base
 import os
 
-class WebSpyder_Mobile(Base):
+
+class WebSpyder_Mobile (Base):
 
     # Start Driver
     def start_driver(self):
         try:
-            chrome_bin_path = self.get_driver_path()
-            chromeOptions = self.configure_chrome_options()
-            driver = webdriver.Chrome(
+            chrome_bin_path = self.get_driver_path ( )
+            chromeOptions = self.configure_chrome_options ( )
+            driver = webdriver.Chrome (
                 executable_path=chrome_bin_path,
                 chrome_options=chromeOptions
             )
@@ -20,24 +21,24 @@ class WebSpyder_Mobile(Base):
             print "WebDriver exception"
             print e
 
-    sleep(4)
+    sleep (4)
 
     @staticmethod
     # Get required driver path
     def get_driver_path():
-        path = os.getcwd()
-        bin_path = os.path.join(path,'chromedriver')
+        path = os.getcwd ( )
+        bin_path = os.path.join (path, 'chromedriver')
         return bin_path
 
     # Configure chrome options for mobile - Includes configuration of User agent
     @staticmethod
     def configure_chrome_options():
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument(
-            "user-agent=Mozilla/5.0 (iPhone; CPU "+
-            "iPhone OS 10_0_1 like Mac OS X) "+
-            "AppleWebKit/602.1.50 (KHTML, like Gecko) "+
+        options = webdriver.ChromeOptions ( )
+        options.add_argument ('--headless')
+        options.add_argument (
+            "user-agent=Mozilla/5.0 (iPhone; CPU " +
+            "iPhone OS 10_0_1 like Mac OS X) " +
+            "AppleWebKit/602.1.50 (KHTML, like Gecko) " +
             "Version/10.0 Mobile/14A403 Safari/602.1"
         )
         return options
